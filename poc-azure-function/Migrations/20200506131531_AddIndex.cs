a017eccf1a91acf75ca0c9e605d40277c2d476c1
y.hirano <yasuakihappy@gmail.com>
2020-05-06 22:54:17 +0900

    Add CreateIndex

---
+++ poc-azure-function/Migrations/20200506131531_AddIndex.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace poc_azure_function.Migrations
+{
+    public partial class AddIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex("idx_messages_on_board_id", "messages", "board_id");
+            // migrationBuilder.CreateIndex("idx_users_on_name_email", "users", new string[] { "name", "email" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex("idx_messages_on_board_id");
+            // migrationBuilder.DropIndex("idx_users_on_name_email");
+        }
+    }
+}
