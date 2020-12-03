b6e6a4390d59a5df101d79494b2c1d7353bbdc7f
y.hirano <yasuakihappy2@yahoo.co.jp>
2020-12-03 17:48:02 +0900

    magick.net test

--- poc-azure-function/Controllers/ImageController.cs
+++ poc-azure-function/Controllers/ImageController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Reflection;
+using System;
 using System.IO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -6,6 +7,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using ImageMagick;
 
 namespace poc_azure_function.Controllers
 {
@@ -38,6 +40,55 @@ namespace poc_azure_function.Controllers
             var bytes = Convert.FromBase64String(base64Image.Value);
 
             return new FileContentResult(bytes, "image/png");
+        }
+
+        [FunctionName("ImageResize")]
+        public static IActionResult ImageToBase64Resize(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "image_resize")] HttpRequest req,
+            ILogger log)
+        {
+            // requestの画像を取得
+            var stream = new StreamReader(req.Body).BaseStream;
+
+            // streamからmemorystreamに変換
+            var ms = new MemoryStream();
+            StreamToMemoryStream(stream, ms);
+
+            var settings = new MagickReadSettings();
+
+            ms.Position = 0;
+            var image = new MagickImage(ms, settings);
+
+            // オリジナル画像の幅を保持
+            int originalImageWith = image.Width;
+
+            // 画像を縮小する
+            var reducedImageMemory = new MemoryStream();
+            image.Resize(originalImageWith / 2, 0);
+            image.Write(reducedImageMemory);
+
+            // 元のサイズに戻す
+            reducedImageMemory.Position = 0;
+            var outputImageMemory = new MemoryStream();
+
+            settings.Width = originalImageWith;
+            var resizeImageMemory = new MagickImage(reducedImageMemory, settings);
+            resizeImageMemory.Strip();
+            resizeImageMemory.Resize(originalImageWith, 0);
+            resizeImageMemory.Write(outputImageMemory);
+
+            var bytes = Convert.FromBase64String(Convert.ToBase64String(outputImageMemory.ToArray()));
+            return new FileContentResult(bytes, "image/png");
+        }
+
+        public static void StreamToMemoryStream(Stream input, MemoryStream output)
+        {
+            byte[] buffer = new byte[32*1024];
+            int read;
+            while((read = input.Read (buffer, 0, buffer.Length)) > 0)
+            {
+                output.Write (buffer, 0, read);
+            }
         }
     }
 }
